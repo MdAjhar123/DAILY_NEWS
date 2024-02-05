a0d46db5963cbb76361fecbc191bf6e37daac8ae
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-06 01:44:25 +0530

    api fetched 26, 27, 28
    fetching api using news api, arranged the pages in proper manner with 20 sets in one page and added next previous buttons

--- src/components/News.js
+++ src/components/News.js
@@ -1,66 +1,74 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
-  articles = [
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-    ]
 
+  constructor() {
+    super();
+    this.state = {
+      articles:[],
+      loading: false,
+      page: 1
+    };
+  }
 
-  constructor(){
-    super()
-    this.state={
-      articles: this.articles,
-      loading: false
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=6ba3ad295a2e4db69de29164cba07c47&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+  }
+
+  handlePrevClick = async()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6ba3ad295a2e4db69de29164cba07c47&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+
+  handleNextClick = async()=>{
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
     }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6ba3ad295a2e4db69de29164cba07c47&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    }
+    
   }
 
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <h1>NewsMonkey - Top HeadLines</h1>
 
         <div className="row">
-          <div className="col-md-3">
-            <NewsItems title="myTitle" description = "myDescription" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" newsUrl="Todo" />
-          </div>
-          <div className="col-md-3">
-            <NewsItems title="myTitle" description = "myDescription" />
-          </div>
-          <div className="col-md-3">
-            <NewsItems title="myTitle" description = "myDescription" />
-          </div>
-          
+          {this.state.articles.map((element) => {
+            return (<div className="col-md-4" key={element.url}>
+                <NewsItems title={element.title ? element.title : ""} description={element.description?element.description :""} imageUrl={element.urlToImage} newsUrl={element.url} />
+              </div>
+            );
+          })}
         </div>
-        
+        <div className="container d-flex justify-content-between" >
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -7,11 +7,11 @@ export class NewsItems extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl ? "https://cdn.mos.cms.futurecdn.net/27APpc5D5KTe7jmBBinq3o-1200-80.jpg" : imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-sm btn-primary">Read more</a>
+            <a href={newsUrl} target='blank' className="btn btn-sm btn-dark">Read more</a>
           </div>
         </div>
       </div>

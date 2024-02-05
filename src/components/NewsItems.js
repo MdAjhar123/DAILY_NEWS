9660ec44942fc7e6477736dc09d77d2e596f485e
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-05 20:22:21 +0530

    added constructor in news folder 25
    added constructor in news folder and created an array with articles name 25

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
+  
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="#" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
       </div>

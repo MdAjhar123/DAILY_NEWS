925b753271107e5bcfc739b979a9855f71e9988c
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-07 23:34:57 +0530

    Fetching news category wise
    Fetching news of all category like business, entertainment, sports etc.
    using router for changing in different category

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
 
 export default class App extends Component {
-
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={5} country="in" category="science"/>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<News pageSize={5} key='general' country='in' category='general'/>}></Route>
+            <Route exact path="/business/" element={<News pageSize={5} key='business' country='in' category='business'/>}></Route>
+            <Route exact path="/entertainment" element={<News pageSize={5} key='entertainment' country='in' category='entertainment'/>}></Route>
+            <Route exact path="/general" element={<News pageSize={5} key='general' country='in' category='general'/>}></Route>
+            <Route exact path="/health" element={<News pageSize={5} key='health' country='in' category='health'/>}></Route>
+            <Route exact path="/science" element={<News pageSize={5} key='science' country='in' category='science'/>}></Route>
+            <Route exact path="/sports" element={<News pageSize={5} key='sports' country='in' category='sports'/>}></Route>
+            <Route exact path="/technology" element={<News pageSize={5} key='technology' country='in' category='technology'/>}></Route>  
+          </Routes>
+        </BrowserRouter>
       </div>
     )
   }

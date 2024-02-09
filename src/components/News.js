dc4661c80964fbfb5b715d5e2e57d3896098773c
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-09 21:44:29 +0530

    Title changed category wise
    Title changed category wise  and top headlines will also change category wise

--- src/components/News.js
+++ src/components/News.js
@@ -19,13 +19,18 @@ export class News extends Component {
 
   }
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - DailyNews`;
   }
 
   async updateNews() {
@@ -57,7 +62,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{ margin: '35px 0px' }}>DailyNews Update- Top HeadLines</h1>
+        <h1 className="text-center" style={{ margin: '35px 0px' }}>DailyNews Update- Top {this.capitalizeFirstLetter(this.props.category)}  HeadLines</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
